b4db9c1438b89dedca51fdcc78977a91dea23785
Sahan Lakmal <sahanloser@gmail.com>
2023-08-13 19:27:07 +0530

    Algo-8 Updated

--- Algo8.java
+++ Algo8.java
@@ -6,10 +6,10 @@ import java.util.Set;
 public class Algo8 {
     public static void main(String[] args) {
         
-        int[] numA = {5, 7, -2, 3, 4, 7, 6,7};
+        int[] numA = {5, 7, -2, 3, 4, 6,-8};
         int[] numB = {7, 8, -8, 2, 1, -9, 6};
 
-        //Create unique numA
+        //!Create unique numA--- This part done by before changing the numA = {5, 7, -2, 3, 4, 6,7}
 
         String str1 = "";
 
@@ -84,19 +84,44 @@ public class Algo8 {
         System.out.println("numA \u222A numB = "+ setA);
 
 
-        //?numA / numB
-        /*
-         * 
-         * 
-         * 
-         */
-        //?numB / numA
-        /*
-         * 
-         * 
-         * 
-         */
+        //!numA / numB = numA - numB
+
+        String str4 = "";
+        loop:
+        for (int i = 0; i < unqA.length; i++) {
+            int k = unqA[i];
+
+            for (int j = 0; j < numB.length; j++) {
+                if((k == numB[j])){
+                    continue loop;
+                }
+            }
+            str4 += k + ",";
+
+        }
+        str4 = str4.substring(0, str4.length()-1);
+        System.out.println("numA / numB = " + "[" + str4 + "]");
         
+
+        //!numB / numA = numB - numA
+
+        String str5 = "";
+        loop:
+        for (int i = 0; i < numB.length; i++) {
+            int k = numB[i];
+
+            for (int j = 0; j < unqA.length; j++) {
+                if((k == unqA[j])){
+                    continue loop;
+                }
+            }
+            str5 += k + ",";
+
+        }
+        str5 = str5.substring(0, str5.length()-1);
+        System.out.println("numB / numA = " + "[" + str5 + "]");
+        
+
         //!numA △ numB
 
 
@@ -112,6 +137,9 @@ public class Algo8 {
             }
         }
         System.out.println(("numA \u25B3 numB = " + setA));
+
+
+        
 
        
 
